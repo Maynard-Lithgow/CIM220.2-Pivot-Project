57915a66e3ab9bac1e668251780839bf6b7867cf
SAE1\1034545 <1034545@student.sae.edu.au>
2025-11-26 17:46:25 +1000

    Added all of Januar's animations

---
+++ Assets/Scripts/AnimationManager.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    //Look at lists from other things to remember
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
